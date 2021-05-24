dfa1a50894fa84d9e215394d3239cbe0b14cdeba
Xuyan Hu <83677943+huxuyan7724@users.noreply.github.com>
2021-05-24 05:26:57 +0100

    克隆代码包

---
+++ README.md
@@ -0,0 +1,14 @@
+# dashboard-carpet2021
+dashboard-carpet2021 created by GitHub Classroom
+README created 17/05/21
+Current Dashboard Functionality: 
+
+  05/05/21 - PROJECT START
+  
+  08/05/21 - Number of Unique Items - changes in views/controllers.py, database/controllers.py and index.html
+  
+  09/05/21 - Testing - changes in test_database.py
+  
+  17/05/21 - Creatinine Clearance Calculator - changes in popup.js and index.html
+
+  19/05/21 - About Information Box - changes in abx.css and index.html
---
+++ app/tests/test_database.py
@@ -0,0 +1,34 @@
+"""
+NAME:          test_database.py
+AUTHOR:        Alan Davies (Lecturer Health Data Science)
+EMAIL:         alan.davies-2@manchester.ac.uk
+DATE:          24/12/2019
+INSTITUTION:   University of Manchester (FBMH)
+DESCRIPTION:   Suite of tests for testing the dashboards database
+               functionality.
+"""
+
+import unittest
+#from app import app
+from app.database.controllers import Database
+
+class DatabaseTests(unittest.TestCase):
+    """Class for testing database functionality and connection."""
+    def setUp(self):
+        """Run prior to each test."""
+        self.db_mod = Database()
+
+    def tearDown(self):
+        """Run post each test."""
+        pass
+
+    def test_get_total_number_items(self):
+        """Test that the total number of itmems returns the correct value."""
+        self.assertEquals(self.db_mod.get_total_number_items(), 8218165)
+
+    def test_get_number_unique_items(self):
+        """Test that the number of unique items returns the correct value"""
+        self.assertEquals(self.db_mod.get_number_unique_items(), 13935)
+
+if __name__ == "__main__":
+    unittest.main()
